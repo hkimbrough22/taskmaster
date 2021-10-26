9239f89e9dab23265b978456731cd60218e0cce5
Haustin Kimbrough <haustin.kimbrough@hotmail.com>
2021-10-25 20:04:30 -0400

    adds Homepage and AddTask activities with functionality

--- app/src/main/java/com/hkimbrough22/taskmaster/MainActivity.java
+++ app/src/main/java/com/hkimbrough22/taskmaster/MainActivity.java
@@ -2,13 +2,30 @@ package com.hkimbrough22.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String SUBMITTED = "Submitted!";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addTaskButton = (Button) findViewById(R.id.homepageAddTaskButton);
+
+        addTaskButton.setOnClickListener(view -> {
+            Intent addTaskIntent = new Intent(MainActivity.this, AddTaskActivity.class);
+            startActivity(addTaskIntent);
+        });
+
+        Button allTasksButton = (Button) findViewById(R.id.homepageAllTasksButton);
+        allTasksButton.setOnClickListener(view -> {
+            Intent allTasksIntent = new Intent(MainActivity.this, AllTasksActivity.class);
+            startActivity(allTasksIntent);
+        });
     }
 }
